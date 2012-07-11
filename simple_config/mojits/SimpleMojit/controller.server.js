f2a6d417ddecb1d091792a3546ce5070e7bf3317
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-07-12 00:47:13 +0800

    added app: simple_config

---
+++ simple_config/mojits/SimpleMojit/controller.server.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('SimpleMojit', function(Y, NAME) {
+
+/**
+ * The SimpleMojit module.
+ *
+ * @module SimpleMojit
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.mojito.controllers[NAME] = {
+
+        init: function(config) {
+            this.config = config;
+        },
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            ac.models.SimpleMojitModelFoo.getData(function(err, data) {
+                if (err) {
+                    ac.error(err);
+                    return;
+                }
+                ac.assets.addCss('./index.css');
+                ac.done({
+                    status: 'Mojito is working.',
+                    data: data
+                });
+            });
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'SimpleMojitModelFoo']});
