78ce1b8c78cff0959f7a15ff9965ce60a8e939f2
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-08-06 16:34:57 +0800

    updated simple_logging

--- simple_logging/mojits/logMojit/binders/index.js
+++ simple_logging/mojits/logMojit/binders/index.js
@@ -1,50 +1,12 @@
-/*
- * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
- */
-/*jslint anon:true, sloppy:true, nomen:true*/
-YUI.add('logMojitBinderIndex', function(Y, NAME) {
-
-/**
- * The logMojitBinderIndex module.
- *
- * @module logMojitBinderIndex
- */
-
-    /**
-     * Constructor for the logMojitBinderIndex class.
-     *
-     * @class logMojitBinderIndex
-     * @constructor
-     */
-    Y.namespace('mojito.binders')[NAME] = {
-
-        /**
-         * Binder initialization method, invoked after all binders on the page
-         * have been constructed.
-         */
-        init: function(mojitProxy) {
-            this.mojitProxy = mojitProxy;
-        },
-
-        /**
-         * The binder method, invoked to allow the mojit to attach DOM event
-         * handlers.
-         *
-         * @param node {Node} The DOM node to which this mojit is attached.
-         */
-        bind: function(node) {
-            var me = this;
-            this.node = node;
-            node.all('dt').on('mouseenter', function(evt) {
-                var dd = '#dd_' + evt.target.get('text');
-                me.node.one(dd).addClass('sel');
-            });
-            node.all('dt').on('mouseleave', function(evt) {
-                var dd = '#dd_' + evt.target.get('text');
-                me.node.one(dd).removeClass('sel');
-            });
-        }
-
-    };
-
-}, '0.0.1', {requires: ['event-mouseenter', 'mojito-client']});
+YUI.add('logBinderIndex', function(Y, NAME) {
+  Y.namespace('mojito.binders')[NAME] = {
+    init: function(mojitProxy) {
+      Y.log('[BINDER]: Log message from init.',"info");
+      this.mojitProxy = mojitProxy;
+    },
+    bind: function(node) {
+      Y.log('[BINDER]: Log message from bind.',"info");
+      this.node = node;
+    }
+  };
+}, '0.0.1', {requires: ['mojito-client']});
