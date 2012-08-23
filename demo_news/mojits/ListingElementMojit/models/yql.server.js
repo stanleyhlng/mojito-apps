2961158e5e0d85781d27e7068125476ce12d2183
Stanley Ng <stanleyn@stanleyn-lm.local>
2012-08-24 01:05:27 +0800

    added model

---
+++ demo_news/mojits/ListingElementMojit/models/yql.server.js
@@ -0,0 +1,13 @@
+YUI.add('YQLModel', function(Y, NAME) {
+
+	Y.mojito.models.yql = {
+
+		init: function(config) {
+			this.config = config;
+		},
+
+		getStories: function(options, start, count, callback) {
+		}		
+	}
+
+}, '0.0.1', {requires: ['yql']});
